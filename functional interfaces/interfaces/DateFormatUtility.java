1e5b0a341536144886a538008eec3716145ecf7e
vanshs1082-wq <vanshs1082@gmail.com>
2025-11-17 10:21:09 +0530

    Delete functional interfaces/interfaces directory

--- functional interfaces/interfaces/DateFormatUtility.java
+++
@@ -1,6 +0,0 @@
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-interface DateFormatUtility {
-    static String format(Date d,String pattern){return new SimpleDateFormat(pattern).format(d);}
-}
--- functional interfaces/interfaces/UnitConversion.java
+++
@@ -1,5 +0,0 @@
-
-interface UnitConversion {
-    static double kmToMiles(double a){return a*0.621371;}
-    static double kgToLbs(double a){return a*2.20462;}
-}
