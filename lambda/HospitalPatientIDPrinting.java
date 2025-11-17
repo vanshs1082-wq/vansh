76e2bdcd7a2f082af0a0dd2ace751515b70ee77b
vanshs1082-wq <vanshs1082@gmail.com>
2025-11-17 10:09:52 +0530

    Delete lambda directory

--- lambda/HospitalPatientIDPrinting.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class HospitalPatientIDPrinting {
-    public static void main(String[] args) {
-        List<Integer> abc = Arrays.asList(101,102,103,104);
-        abc.forEach(System.out::println);
-    }
-}
