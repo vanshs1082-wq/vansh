ff2ef4f8bd8f0e970ff9463dad05e1c74ea7b81b
vanshs1082-wq <vanshs1082@gmail.com>
2025-11-17 10:28:24 +0530

    Delete lambda directory

--- lambda/NameUppercasing.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class NameUppercasing {
-    public static void main(String[] args) {
-        List<String> xyz = Arrays.asList("ram","shyam","mohan");
-        xyz.stream().map(String::toUpperCase).forEach(System.out::println);
-    }
-}
