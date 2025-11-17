54fc403c79ae02332901f5b22a19ac2a0d1a3aa9
vanshs1082-wq <vanshs1082@gmail.com>
2025-11-17 10:28:03 +0530

    Delete functional interfaces/interfaces directory

--- functional interfaces/interfaces/StringLengthChecker.java
+++
@@ -1,6 +0,0 @@
-
-import java.util.function.Function;
-class StringLengthChecker {
-    Function<String,Integer> f = s -> s.length();
-    void check(String a,int limit){if(f.apply(a)>limit) System.out.println("Too Long"); else System.out.println("OK");}
-}
--- functional interfaces/interfaces/TemperatureAlert.java
+++
@@ -1,6 +0,0 @@
-
-import java.util.function.Predicate;
-class TemperatureAlert {
-    Predicate<Double> p = t -> t>50;
-    void check(double a){if(p.test(a)) System.out.println("Alert"); else System.out.println("Normal");}
-}
